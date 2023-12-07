2f92b1151606e2298230a51d880b076f308de24f
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-12-07 20:28:52 +0530

    first commit

---
+++ 6.py
@@ -0,0 +1,13 @@
+a="python"
+b=[1,2,3]
+aTup=(a,b)
+print(aTup[1][1:])
+
+
+#a = 'python' -> python 
+#b = [1, 2, 3] -> [1, 2, 3] 
+#aTup = (a, b) -> ('python', [1, 2, 3]) ->  <class 'tuple'> 
+#-> aTup[0] = python, aTup[1] = [1, 2, 3], and aTup[1][1:] =[2, 3]
+#print(aTup[1][1:]) -> [2, 3]  
+
+#Thus, answer is [2, 3]
